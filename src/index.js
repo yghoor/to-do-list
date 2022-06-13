5cdfb2456855b33409f20b9fb70cb21b53177b4b
Yahya Ghoor <ghooryahya@gmail.com>
2022-06-13 13:49:34 +0200

    Create array of objects containing tasks

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const tasks = [
+  {
+    description: 'do task #1',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'do task #2',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'do task #3',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'do task #4',
+    completed: false,
+    index: 3,
+  },
+];
