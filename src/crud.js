511282729a7d053e7eeedc3d0e0116ed9d5f2473
Yahya Ghoor <ghooryahya@gmail.com>
2022-06-15 14:48:04 +0200

    Add function to add event listener for task edit

--- src/crud.js
+++ src/crud.js
@@ -6,3 +6,65 @@ export function addTask(tasksArray, currentItemInput) {
   });
 }
 
+export function addInputEditor(task, tasksArray) {
+  const currentItem = document.getElementById(`item-${task.index}`);
+  const currentItemText = currentItem.children[1];
+  const menuButton = currentItem.children[2];
+  const deleteButton = currentItem.children[3];
+
+  currentItemText.addEventListener('click', () => {
+    const taskEditor = document.createElement('input');
+    taskEditor.type = 'text';
+    taskEditor.className = 'edit-task';
+    taskEditor.value = task.description;
+
+    if (currentItemText.innerHTML === task.description) {
+      currentItemText.innerHTML = '';
+      currentItemText.appendChild(taskEditor);
+
+      menuButton.classList.add('hidden');
+      deleteButton.classList.remove('hidden');
+      deleteButton.classList.add('shown');
+
+      currentItem.classList.add('bg-yellow');
+    }
+
+    taskEditor.focus();
+
+    taskEditor.addEventListener('keypress', (event) => {
+      if (event.key === 'Enter') {
+        task.description = `${taskEditor.value}`;
+
+        if (taskEditor.value === '') {
+          removeTask(task, tasksArray);
+        }
+
+        saveTasksToLocalStorage(tasksArray);
+
+        menuButton.classList.remove('hidden');
+        deleteButton.classList.add('hidden');
+        deleteButton.classList.remove('shown');
+        currentItem.classList.remove('bg-yellow');
+
+        currentItemText.textContent = `${taskEditor.value}`;
+      }
+    });
+
+    taskEditor.addEventListener('blur', () => {
+      task.description = `${taskEditor.value}`;
+
+      if (taskEditor.value === '') {
+        removeTask(task, tasksArray);
+      }
+
+      saveTasksToLocalStorage(tasksArray);
+
+      menuButton.classList.remove('hidden');
+      deleteButton.classList.add('hidden');
+      deleteButton.classList.remove('shown');
+      currentItem.classList.remove('bg-yellow');
+
+      currentItemText.textContent = `${taskEditor.value}`;
+    });
+  });
+}
