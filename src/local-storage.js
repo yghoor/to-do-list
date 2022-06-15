bbed4723b21f7dfe18286c0176fff72a7b4925f3
Yahya Ghoor <ghooryahya@gmail.com>
2022-06-15 14:44:10 +0200

    Create function to check if local storage exists

--- src/local-storage.js
+++ src/local-storage.js
@@ -2,3 +2,9 @@ export function saveTasksToLocalStorage(tasksArray) {
   localStorage.setItem('tasks', JSON.stringify(tasksArray));
 }
 
+export function inspectLocalStorage() {
+  if (localStorage.getItem('tasks')) {
+    return true;
+  }
+  return false;
+}
