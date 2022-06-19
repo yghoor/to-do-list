47930bfa803c98645a00dcd181d85c6e17c7cc38
Yahya Ghoor <ghooryahya@gmail.com>
2022-06-19 19:29:07 +0200

    Set up document and create variables for testing

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,3 +4,33 @@
 
 import { addTask } from '../src/crud.js';
 
+document.body.innerHTML = `
+  <div class="add-item">
+    <button type="button" class="enter-button">&#8629;</button>
+  </div>
+  <ul id="list-items"></ul>`;
+
+const enterButton = document.querySelector('.enter-button');
+const itemsList = document.getElementById('list-items');
+
+let tasks = [
+  {
+    description: 'Task 1',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Task 2',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Task 3',
+    completed: false,
+    index: 3,
+  },
+];
+
+let storage = [];
+
+const itemInput = 'Task-4';
