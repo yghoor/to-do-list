ac0bb8e5b7eacae7f9a0c21cbbbe873cd17ca0c0
Yahya Ghoor <ghooryahya@gmail.com>
2022-06-20 12:50:37 +0200

    Mock task array renumber function

--- __tests__/update-edit-and-clear-tasks.test.js
+++ __tests__/update-edit-and-clear-tasks.test.js
@@ -37,3 +37,10 @@ function saveToStorage(tasksArray) {
   storage = tasksArray;
 }
 
+function renumberTaskIndexes(tasksArray) {
+  tasksArray.forEach((task, taskNum) => {
+    task.index = taskNum + 1;
+  });
+  saveToStorage(tasksArray);
+}
+
